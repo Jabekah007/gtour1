87919a429545bbcc15946d63d02643e0b376d2bf
Jabekah007 <70330339+Jabekah007@users.noreply.github.com>
2022-11-25 16:54:13 +0000

    refactor

---
+++ src/Tours.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Tour from './Tour';
+/**
+ * We're mapping over the tours array and returning a Tour component for each tour
+ * @returns The tours component is being returned.
+ */
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
